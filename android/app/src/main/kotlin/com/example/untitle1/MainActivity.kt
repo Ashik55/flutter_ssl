d47607c33cc63986c4af185d674771de9151d2dd
Aashiq <ashik.cse.developer@gmail.com>
2021-11-11 12:34:22 +0600

    initial commit by ashiq

---
+++ android/app/src/main/kotlin/com/example/untitle1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.untitle1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sslcommerz/flutter_sslcommerz.dart';
+import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
+import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
+import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
+import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
+import 'package:flutter_sslcommerz/sslcommerz.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () => callSSL(), child: Text("click me"))),
+    );
+  }
+
+  callSSL()async {
+
+
+
+    //https://github.com/sslcommerz/SSLCommerz-Flutter/blob/master/example/lib/main.dart
+
+    Sslcommerz sslcommerz = Sslcommerz(
+        initializer: SSLCommerzInitialization(
+          //   ipn_url: "www.ipnurl.com",
+            multi_card_name: "visa,master,bkash",
+            currency: SSLCurrencyType.BDT,
+            product_category: "Food",
+            sdkType: SSLCSdkType.TESTBOX,
+            store_id: "shukr608665a142181",
+            store_passwd: "shukr608665a142181@ssl",
+            total_amount: 100.20,
+            tran_id:  "15112s121s2s12s1"));
+
+    var result = await sslcommerz.payNow();
+
+    print(result);
+    if (result is PlatformException) {
+      print("the response is: " +
+          result.message.toString() +
+          " code: " +
+          result.code);
+    } else {
+      SSLCTransactionInfoModel model = result;
+      print("Transaction successful: Amount ${model.amount} TK");
+    }
+
+  }
+}
